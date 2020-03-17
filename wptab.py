efec71f96a6669bbb9dc3180e28f855946a08d28
Anton Hardock <antonhardock@web.de>
2020-03-17 20:42:38 +0100

    made input options for table search more versatile

--- wptab.py
+++ wptab.py
@@ -1,32 +1,43 @@
 ## branch flexible-table-search
+## allow search by row or header
 import bs4 as bs
 import urllib.request
+import pandas as pd
 
-def get_tab(url, col_id, col_vals, wikitable_class='wikitable sortable'):
+def get_tab(source, t_vals, search="head", t_id=0, wikitable_class='wikitable sortable'):
     '''Find a specific table from a wikipedia url and return it as a list of lists.
-    Each list represents a table row, including the header. To target a specific table,
-    two hints are required: A column id (col_id, zero based) and a set of one or more values that column contains (col_vals)
-    The function parses all wikipedia tables of a given table class from top to bottom (default: wikitable sortable).
-    For each table, only the specified column is parsed and searched for the target values.
-    The search ends with the first match.
+    Source: either  be url or instance of bs.BeautifulSoup() (remove when tests are done)
+    Each list represents a table row, including the header. RENEW THIS PART
+    Default is looking through header
+    If column or row is targeted, default col/row Id is 0. 
     '''
-    sauce = urllib.request.urlopen(url).read()
-    soup = bs.BeautifulSoup(sauce, 'lxml')
     target = list()
 
-    for table in soup.find_all('table', class_= wikitable_class):
-        
-        ## first row is skipped since it usually contains table header tags (th) and no table data tags (td)
-        ## str.strip() required since additional whitespace + newline chars might be hidden in the html source
-        column = (row.find_all('td')[col_id].text.strip() for row in table.find_all('tr')[1:]) 
+    if not isinstance(source, bs.BeautifulSoup): 
+        source = urllib.request.urlopen(source).read()
+        source = bs.BeautifulSoup(source, 'lxml')
 
-        ## if firstrow contains all of the target values, extract table
-        if col_vals.issubset(set(column)):
+    for table in source.find_all('table', class_= wikitable_class):
+        
+        ## str.strip() required for parsing since additional whitespace + newline chars might be hidden in the html source 
+        if search == "head":
+            ## first row is skipped since it usually contains table header tags (th) and no table data tags (td)
+            x = (th.text.strip() for th in table.find_all('th'))
+        elif search == "row":
+            t_id += 1 #add one since the header will be the first row found
+            x = (d.text.strip() for d in table.find_all('tr')[t_id].find_all('td'))
+        elif search == "column":
+            x = (row.find_all('td')[t_id].text.strip() for row in table.find_all('tr')[1:])
+        else:
+            raise ValueError("Parameter 'search' must be 'head', 'row', or 'column'")
+
+        ## if target head/row/column contains all of the target values, extract table
+        if t_vals.issubset(set(x)):
             target = table
             break 
 
     ## if target is an empty list, raise exception
-    if not target: raise ValueError("Specified table not found. Maybe try another wikitable class?")
+    if not target: raise ValueError("No table found. Try other target values or another wikitable class")
 
     ## parse the table data into a list of lists
     rows = list()
@@ -40,5 +51,5 @@ def get_tab(url, col_id, col_vals, wikitable_class='wikitable sortable'):
     # get the header and insert as first row
     header = [th.text.strip() for th in target.find_all('th')]    
     rows.insert(0, header)
-
+    
     return rows
