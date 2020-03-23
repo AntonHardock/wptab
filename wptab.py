b1aecc3989991786e44b46eca9fe18e2888aa179
Anton Hardock <antonhardock@web.de>
2020-03-23 19:30:56 +0100

    Added table search by header, row or colum

--- wptab.py
+++ wptab.py
@@ -1,54 +1,59 @@
-## branch flexible-table-search
-## allow search by row or header
 import bs4 as bs
 import urllib.request
 import pandas as pd
 
-def get_tab(source, t_vals, search="head", t_id=0, wikitable_class='wikitable sortable'):
-    '''Find a specific table from a wikipedia url and return it as a list of lists.
-    Source: either  be url or instance of bs.BeautifulSoup() (remove when tests are done)
-    Each list represents a table row, including the header. RENEW THIS PART
-    Default is looking through header
-    If column or row is targeted, default col/row Id is 0. 
+def get_tab(url, t_vals, search="head", t_id=0, wikitable_class='wikitable sortable'):
+    '''Extract a specific table from a wikipedia URL and return it as a list of lists.
+    Each nested list represents a table row, including the header.
+    Tables of a predefined html class (default 'wikitable sortable') are searched from top to bottom.
+    Find a specific table requires a set object of target values (t_vals).
+    Those values can be looked up in each header (default), 
+    or in a particular row / column of each table (search='row' / search='column').
+    The latter options require a row / column id (t_id, default=0). Ids are zero based.
+    For example, search="row" and t_id=2 will go through the third row of each table.
     '''
+    sauce = urllib.request.urlopen(url).read()
+    soup = bs.BeautifulSoup(sauce, 'lxml')
     target = list()
 
-    if not isinstance(source, bs.BeautifulSoup): 
-        source = urllib.request.urlopen(source).read()
-        source = bs.BeautifulSoup(source, 'lxml')
-
-    for table in source.find_all('table', class_= wikitable_class):
+    for table in soup.find_all('table', class_= wikitable_class):
         
-        ## str.strip() required for parsing since additional whitespace + newline chars might be hidden in the html source 
+        ## extract data entries from header or target row / target column
         if search == "head":
-            ## first row is skipped since it usually contains table header tags (th) and no table data tags (td)
-            x = (th.text.strip() for th in table.find_all('th'))
+            x = (h.text.strip() for h in table.find_all('th')) #strip() required for parsing as source code might contain additional whitespace + newline chars
         elif search == "row":
-            t_id += 1 #add one since the header will be the first row found
-            x = (d.text.strip() for d in table.find_all('tr')[t_id].find_all('td'))
+            try:
+                rows = table.find_all('tr')[1:] #first row excluded as it is usually empty (containing th, but not td tags)
+                x = (d.text.strip() for d in rows[t_id].find_all('td'))
+            except IndexError:
+                continue
         elif search == "column":
-            x = (row.find_all('td')[t_id].text.strip() for row in table.find_all('tr')[1:])
+            try:
+                rows = table.find_all('tr')[1:] #first row excluded as it is usually empty (containing th, but not td tags)
+                x = (d.find_all('td')[t_id].text.strip() for d in rows)
+            except IndexError:
+                continue
         else:
             raise ValueError("Parameter 'search' must be 'head', 'row', or 'column'")
-
-        ## if target head/row/column contains all of the target values, extract table
+        
+        ## if target values are subset of extracted data, keep table and break loop 
         if t_vals.issubset(set(x)):
             target = table
-            break 
-
-    ## if target is an empty list, raise exception
+            break
+        
+    ## if target remained an empty list, raise error
     if not target: raise ValueError("No table found. Try other target values or another wikitable class")
-
+        
     ## parse the table data into a list of lists
     rows = list()
     for row in target.find_all('tr'):
         row = [d.text.strip() for d in row.find_all('td')]
         rows.append(row)
 
-    # remove first row if it is an empty list
+    ## remove first row if it is an empty list
     if not rows[0]: del rows[0] 
 
-    # get the header and insert as first row
+    ## get the header and insert as first row
     header = [th.text.strip() for th in target.find_all('th')]    
     rows.insert(0, header)
     
